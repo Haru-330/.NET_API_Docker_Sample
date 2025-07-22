ca04cf54648a1777a49b634fa8668ebd78925832
Haru-330 <haruna.330romiyaneiro@gmail.com>
2025-07-22 22:09:09 +0900

    fix: GET,POST,Put,Deleteを追加

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -17,7 +17,46 @@ public class BooksController : ControllerBase
     [HttpGet]
     public ActionResult<IEnumerable<Book>> List()
     {
-        var books = context.Books;
-        return books;
+        var books = new { items = context.Books };
+        return Ok(books);
+    }
+    [HttpGet("{id}")]
+    public ActionResult<Book> Get(int id)
+    {
+        var book = context.Books.Find(id);
+        if (book == null)
+        {
+            return NotFound();
+        }
+        return Ok(book);
+    }
+    [HttpPost]
+    public void Post([FromBody] Book book)
+    {
+        context.Books.Add(book);
+        context.SaveChanges();
+    }
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Book book)
+    {
+        var target = context.Books.Find(id);
+        if (target == null)
+        {
+            return;
+        }
+        target.Title = book.Title;
+        target.Author = book.Author;
+        context.SaveChanges();
+    }
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+        var book = context.Books.Find(id);
+        if (book == null)
+        {
+            return;
+        }
+        context.Books.Remove(book);
+        context.SaveChanges();
     }
 }
